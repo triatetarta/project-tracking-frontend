38e0fdd7fed1512420fdcd6c7e700c582ff8a584
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2022-05-01 13:30:09 +0100

    env name change

--- src/Comments/commentService.js
+++ src/Comments/commentService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${process.env.API_URL}/api/tickets/`;
+const API_URL = `${process.env.REACT_APP_API_URL}/api/tickets/`;
 
 // Get ticket comments
 const getComments = async (ticketId, token) => {
--- src/Projects/projectService.js
+++ src/Projects/projectService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${process.env.API_URL}/api/projects/`;
+const API_URL = `${process.env.REACT_APP_API_URL}/api/projects/`;
 
 // Create new project
 const createProject = async (projectData, token) => {
